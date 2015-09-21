a8162a19512b16bf4293f8c95c7c09a0d455545d
Norven <norven63@gmail.com>
2015-09-21 14:12:36 +0800

    分支初体验

--- helloguava/src/main/java/com/helloguava/MainActivity.java
+++ helloguava/src/main/java/com/helloguava/MainActivity.java
@@ -3,8 +3,6 @@ package com.helloguava;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.google.common.io.Files;
