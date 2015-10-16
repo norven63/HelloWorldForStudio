e359751d907317ce03980671622a60222359bc2a
Norven <norven63@gmail.com>
2015-10-16 10:31:38 +0800

    添加动画相关的示例

---
+++ helloworld/src/main/java/com/myAndroid/helloworld/activity/WeiXinChatActivity.java
@@ -0,0 +1,15 @@
+package com.myAndroid.helloworld.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.myAndroid.helloworld.R;
+
+public class WeiXinChatActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_weixin_chat);
+    }
+}
