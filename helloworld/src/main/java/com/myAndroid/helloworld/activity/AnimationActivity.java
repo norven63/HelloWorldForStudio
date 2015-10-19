6569277920a57511afb08039cee256aa65d1bf2e
Norven <norven63@gmail.com>
2015-10-19 10:38:01 +0800

    新增移动动画、动画加速器

--- helloworld/src/main/java/com/myAndroid/helloworld/activity/AnimationActivity.java
+++ helloworld/src/main/java/com/myAndroid/helloworld/activity/AnimationActivity.java
@@ -17,51 +17,61 @@ import com.myAndroid.helloworld.R;
 
 public class AnimationActivity extends Activity {
 
-  @SuppressLint("NewApi")
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_anim);
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anim);
 
-    /*
-     * 缩放动画
-     */
-    View scaleView = findViewById(R.id.scale);
-    Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
-    scaleAnimation.setRepeatCount(Animation.INFINITE);
-    scaleView.startAnimation(scaleAnimation);
+        /*
+        * 缩放动画
+        */
+        View scaleView = findViewById(R.id.scale);
+        Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
+        scaleAnimation.setRepeatCount(Animation.INFINITE);
+        scaleView.startAnimation(scaleAnimation);
 
-    /*
-     * 旋转动画
-     */
-    View rotateView = findViewById(R.id.rotate);
-    Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
-    rotateView.startAnimation(rotateAnimation);
+        /*
+        * 旋转动画
+        */
+        View rotateView = findViewById(R.id.rotate);
+        Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+        rotateView.startAnimation(rotateAnimation);
 
-    final TextView rotationYView = (TextView) findViewById(R.id.rotationY);
-    rotationYView.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
+        /*
+         * 移动动画
+         */
+        View translate = findViewById(R.id.translate);
+        Animation translateAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_translate);
+        translate.startAnimation(translateAnimation);
+
+        /*
+         * 翻转动画
+         */
+        final TextView rotationYView = (TextView) findViewById(R.id.rotationY);
+        rotationYView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotation(rotationYView);
+            }
+        });
         rotation(rotationYView);
-      }
-    });
-    rotation(rotationYView);
-  }
+    }
 
-  /*
-   * 翻转动画
-   */
-  private void rotation(final View rotationYView) {
-    rotationYView.animate().rotationY(90).setDuration(1000)
-          // 设置加速器，为了动画衔接时保证良好的流畅度
-          .setInterpolator(new AccelerateInterpolator()).setListener(new AnimatorListenerAdapter() {
+    /*
+     * 翻转动画
+     */
+    private void rotation(final View rotationYView) {
+        rotationYView.animate().rotationY(90).setDuration(1000)
+                // 设置加速器，为了动画衔接时保证良好的流畅度
+                .setInterpolator(new AccelerateInterpolator()).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator anim) {
-              rotationYView.setRotationY(-90);// 需要先置为-90度，否者整个View将会反过来
-              rotationYView.animate().rotationY(0).setDuration(1000)
-                    // 设置加速器，为了动画衔接时保证良好的流畅度
-                    .setInterpolator(new DecelerateInterpolator()).setListener(null);// 由-90度转到0度，则View是正的
+                rotationYView.setRotationY(-90);// 需要先置为-90度，否者整个View将会反过来
+                rotationYView.animate().rotationY(0).setDuration(1000)
+                        // 设置加速器，为了动画衔接时保证良好的流畅度
+                        .setInterpolator(new DecelerateInterpolator()).setListener(null);// 由-90度转到0度，则View是正的
             }
-          });
-  }
+        });
+    }
 }
